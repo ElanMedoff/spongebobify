7aab47a641441219beb880290d83b34756678df5
Elan <54599408+ElanMedoff@users.noreply.github.com>
2021-06-13 11:33:42 -0400

    first commit, boilerplate from site blocker

---
+++ src/popup.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./comps/Root";
+import "./popup.css";
+
+ReactDOM.render(<Root />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const config = {
+  entry: {
+    popup: path.join(__dirname, "src/popup.tsx"),
+  },
+  mode: "development",
+  // gets rid of eval error, weird chrome extension thing
+  devtool: "cheap-module-source-map",
+  output: { path: path.join(__dirname, "dist"), filename: "[name].js" },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-modules-typescript-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".css"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "." }],
+    }),
+  ],
+};
+
+module.exports = config;
